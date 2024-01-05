d1313041001eedec8fdbb5abe1be43497c837c00
Julie Goncalves <julie.g27@hotmail.fr>
2024-01-05 07:40:39 +0100

    Nettoyqge du code
    - Suppression des commentaires non pertinents
    - Suppression des logs
    - Correction du bug sur le menu

--- js/menu.js
+++ js/menu.js
@@ -1,26 +1,19 @@
+console.log("ici");
 (function () {
   var focusZoneWidth = "100%";
   var focusZoneHeight = 40;
-
   var isMouseOutsideContainer = true;
 
   function detectMouseMove() {
-    // Initial container width
     var containerHeight = $(".container").outerHeight();
     var containerWidth = $(".container").outerWidth();
-
-    // quand je sors
     var containerMenu = document.querySelector(".container");
 
+    console.log(containerMenu);
     containerMenu.addEventListener("mouseout", function (eventMouseMove) {
-      // document.addEventListener("mousemove", function (eventMouseMove) {
       const position = $(".container").offset();
       theTop = position.top;
       theLeft = position.left;
-      // console.log(theLeft);
-
-      console.log("a l exterieur horizontal");
-      // Quand ma souris est a l exterieur de notre container on veut que notre zone de floue haute prenne toute la hauteur du container, et la zone de floue du bas soit a 0.
 
       isMouseOutsideContainer = true;
       $(".top-side").css({
@@ -31,30 +24,24 @@
       });
     });
 
-    containerMenu.addEventListener("mouseenter", function (eventMouseMove) {
+    containerMenu.addEventListener("mousemove", function (eventMouseMove) {
       const position = $(".container").offset();
       theTop = position.top;
       theLeft = position.left;
-      // quand je suis dans le menu
-      // mouse mouve
-      console.log("a l interieur horizontal");
-      // event.page = evenement qui recupere la position de ma souris
+
       $(".hover").css({
-        top: eventMouseMove.pageY - theTop, // On veut que la cible commence verticalement au niveau de la souris
+        top: eventMouseMove.pageY - theTop,
       });
       $(".top-side").css({
-        height: eventMouseMove.pageY - theTop - focusZoneHeight / 2, // On veut que la zone du haut s'arrete verticalement au niveau de la souris
+        height: eventMouseMove.pageY - theTop - focusZoneHeight / 2,
       });
 
-      // On calcule la nouvelle hauteur de la zone du bas
       var bottomHeight =
         containerHeight -
         (eventMouseMove.pageY - theTop + focusZoneHeight - focusZoneHeight / 2);
       $(".bottom-side").css({
         top:
-          focusZoneHeight + eventMouseMove.pageY - theTop - focusZoneHeight / 2, // nouvelle posisition verticale de la zone du bas
-        // cad, la position de la souris plus la hauteur de la zone nette
-        height: bottomHeight, // on applique
+          focusZoneHeight + eventMouseMove.pageY - theTop - focusZoneHeight / 2,
       });
     });
   }
